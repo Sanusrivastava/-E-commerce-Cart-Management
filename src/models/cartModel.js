376e225e73bc0ca16ff5ec6964a459395d843c51
sanu <shanuksrivastava@gmail.com>
2023-02-08 14:36:40 +0530

    changes in package.json

--- src/models/cartModel.js
+++ src/models/cartModel.js
@@ -3,18 +3,18 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const cartSchema = new mongoose.Schema({            //POST /users/:userId/cart (Add to cart)
 
-    userId      :{type:ObjectId,    required:true,  unique:true,    ref:'User'},
+    userId: { type: ObjectId, required: true, unique: true, ref: 'User' },
 
-    items       :[{
-        productId:{type:ObjectId,    required:true,  ref:'Product'},
-        quantity :{type:Number,      required:true,  default:1},
-        _id      :false
-                }],
+    items: [{
+        productId: { type: ObjectId, required: true, ref: 'Product' },
+        quantity: { type: Number, required: true, default: 1 },
+        _id: false
+    }],
 
-    totalPrice  :{type:Number,      required:true},
-    totalItems  :{type:Number,      required:true}
+    totalPrice: { type: Number, required: true },
+    totalItems: { type: Number, required: true }
 
-}, {timestamps:true});
+}, { timestamps: true });
 
 module.exports = mongoose.model('Cart', cartSchema);
 
